a358ef84923be103ad0a890159be59423730ab9b
Green <shren0812@gmail.com>
2022-06-08 16:14:21 +0900

    #1.3 @Args

--- src/restraurants/restaurants.resolver.ts
+++ src/restraurants/restaurants.resolver.ts
@@ -1,13 +1,10 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entity';
 
-@Resolver()
-// @Resolver(of => Restaurant)
-// 둘 중 어느 방식으로 작성해도 문제없다.
-// 중요한 것은 바로 밑에서 작성하는 @Query의 argument이다.
+@Resolver((of) => Restaurant)
 export class RestaurantsResolver {
-  @Query(() => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query(() => [Restaurant])
+  Restaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
   }
 }
