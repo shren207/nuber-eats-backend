930a10a2d39f0792914955a97ee494930a9d5e23
Green <shren0812@gmail.com>
2022-06-08 20:07:50 +0900

    주석 작성

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // 이런식으로만 작성해 주어도 class-validator가 정상적으로 작동한다!
   await app.listen(3000);
 }
 bootstrap();
