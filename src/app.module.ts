abc7b32978e57fec69d704dbf841e9b01672ec4a
Green <shren0812@gmail.com>
2022-06-08 15:37:09 +0900

    #1.2 ObjectType

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { RestraurantsModule } from './restraurants/restraurants.module';
+
+console.log(process.cwd());
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: true,
     }),
+    RestraurantsModule,
   ],
   controllers: [],
   providers: [],
