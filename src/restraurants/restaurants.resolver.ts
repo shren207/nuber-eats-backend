a98f7738149cbfc6dd5d8af2e02327e9e205023e
Green <shren0812@gmail.com>
2022-06-08 17:58:15 +0900

    #1.4 @InputType(), @ArgsType()

--- src/restraurants/restaurants.resolver.ts
+++ src/restraurants/restaurants.resolver.ts
@@ -1,10 +1,15 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 
 @Resolver((of) => Restaurant)
 export class RestaurantsResolver {
   @Query(() => [Restaurant])
-  Restaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+  restaurant(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    return true;
   }
 }
